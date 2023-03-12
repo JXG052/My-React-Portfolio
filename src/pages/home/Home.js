8944972b4de312d82dfa2abf616f9ca980804d65
JXG052 <jxg0521@gmail.com>
2023-03-12 12:43:53 +0000

    Added basic function and style to Home Page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Profile from './profile.png'
+import './home.css'
 
 function Home() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="home-section">
+      <div className="greeting-section">      
+        <p>Hi There, I'm Jon Green, a Front-End Web Developer</p>
+        <button>About me</button>
+      </div>   
+      <img src={Profile} style={{maxHeight: "300px"}} alt="profile" />
     </div>
+      
+     
   );
 }
 
