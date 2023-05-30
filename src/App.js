b19b8eb33c5259c827d211d4fa66ac137a859a35
JXG052 <jxg0521@gmail.com>
2023-05-30 11:19:32 +0100

    Added Material UI Navbar, style and theme

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import './App.css';
+import React from 'react'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/home/Home.js';
+// import Portfolio from './pages/portfolio/Portfolio';
+// import Contact from './pages/contact/Contact';
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/index';
+import DrawerAppBar from './components/DrawerAppBar/DrawerAppBar';
+
+
+function App() {
+  return (
+    <>
+      <DrawerAppBar />
+    </>
+  //   <Router>
+  //   <div>
+      
+      
+  //     {/* Wrap Route elements in a Routes component */}
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="Portfolio" element={<Portfolio />} />
+  //       <Route path="Contact" element={<Contact />} />
+  //     </Routes>
+  //     <Footer />
+  //   </div>
+  // </Router>
+  );
+}
+
+export default App;
---
+++ src/components/DrawerAppBar/DrawerAppBar.js
@@ -0,0 +1,166 @@
+// import React from 'react';
+// import './NavTabs.css'
+// import { NavLink } from 'react-router-dom';
+import * as React from 'react';
+// import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useStyles } from './style';
+
+// import './DrawerAppBar.css'
+// import { useTheme } from '@mui/material/styles';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const classes = useStyles();
+ 
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box className={classes.grey} onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box  sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" className={classes.grey}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  )
+}
+export default DrawerAppBar;
+
+// PREVIOUS CODE:
+
+{/* <ul className="nav nav-tabs">
+  <li className="nav-item">
+    <NavLink
+      to="/"
+      end
+      className={({ isActive }) =>
+        isActive ? 'nav-link active' : 'nav-link'
+      }
+    // style={({isActive}) => {
+    //   return{
+    //    fontWeight: isActive ? "bold" : "",
+    //    color: isActive? "Green" : "blue",
+    //   };
+    //  }}
+    >
+      Home
+    </NavLink>
+  </li>
+  <li className="nav-item">
+    <NavLink
+      to="Portfolio"
+      className={({ isActive }) =>
+        isActive ? 'nav-link active' : 'nav-link'
+      }
+
+    // style={({isActive}) => {
+    //  return{
+    //   fontWeight: isActive ? "bold" : "",
+    //   color: isActive? "Green" : "blue",
+    //  };
+    // }}
+    >
+      Portfolio
+    </NavLink>
+  </li>
+  <li className="nav-item">
+    <NavLink
+      to="Contact"
+      className={({ isActive }) =>
+        isActive ? 'nav-link active' : 'nav-link'
+      }
+    // style={({isActive}) => {
+    //   return{
+    //    fontWeight: isActive ? "bold" : "",
+    //    color: isActive? "Green" : "blue",
+    //   };
+    //  }}
+    >
+      Contact
+    </NavLink>
+  </li>
+
+</ul> */}
